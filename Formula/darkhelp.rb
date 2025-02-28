b754b90724384ff94055e4892f6b6933ddcc3f70
Julian Dominguez <45999377+Seedname@users.noreply.github.com>
2025-02-28 14:49:04 -0500

    add formulas for darknet-hankai, darkhelp, and darkmark

---
+++ Formula/darkhelp.rb
@@ -0,0 +1,32 @@
+class Darkhelp < Formula
+  desc "DarkHelp: C++ helper class for Darknet's C API"
+  homepage "https://github.com/stephanecharette/DarkHelp"
+  url "https://github.com/stephanecharette/DarkHelp.git",
+      tag:      "v1.9.6",
+      revision: "HEAD"
+  version "1.9.6"
+  license "MIT"
+  head "https://github.com/stephanecharette/DarkHelp.git", branch: "master"
+
+  depends_on "cmake" => :build
+  depends_on "git" => :build
+
+  depends_on "darknet-hankai"
+
+  depends_on "tclap"
+  depends_on "opencv"
+
+  def install
+    mkdir "build" do
+      system "cmake", "..",
+                      "-DCMAKE_BUILD_TYPE=Release",
+                      "-DCMAKE_INSTALL_PREFIX=#{prefix}"
+      system "make", "-j#{ENV.make_jobs}"
+      system "make", "install"
+    end
+  end
+
+  test do
+    system "false"
+  end
+end
---
+++ Formula/darkmark.rb
@@ -0,0 +1,38 @@
+class Darkmark < Formula
+  desc "DarkMark: GUI for Darknet and DarkHelp"
+  homepage "https://github.com/stephanecharette/DarkMark"
+  url "https://github.com/jpfleischer/DarkMark.git",
+      tag:      "v1.10.18",
+      revision: "HEAD"
+  version "1.10.18"
+  license "MIT"
+  head "https://github.com/jpfleischer/DarkMark.git", branch: "master"
+
+  depends_on "cmake" => :build
+  depends_on "git" => :build
+
+  depends_on "darkhelp"
+  depends_on "darknet-hankai"
+  depends_on "opencv"
+
+  depends_on "libx11"
+  depends_on "freetype"
+  depends_on "libxrandr"
+  depends_on "libxinerama"
+  depends_on "libxcursor"
+  depends_on "poppler"
+
+  def install
+    mkdir "build" do
+      system "cmake", "..",
+                      "-DCMAKE_BUILD_TYPE=Release",
+                      "-DCMAKE_INSTALL_PREFIX=#{prefix}"
+      system "make", "-j#{ENV.make_jobs}"
+      system "make", "install"
+    end
+  end
+
+  test do
+    system "false"
+  end
+end
